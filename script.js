5ed7c3d0c917fb87b1b73a3f54289f8798ae23c7
Compass <ndphong165@gmail.com>
2022-07-29 00:12:57 +0700

    push form login

--- script.js
+++ script.js
@@ -6,7 +6,25 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
-window.onscroll = () =>{
+document.querySelector("#login-btn").onclick = () => {
+    document.querySelector('.login-form-container').classList.toggle('active');
+}
+
+document.querySelector("#close-login-form").onclick = () => {
+    document.querySelector('.login-form-container').classList.remove('active');
+}
+
+window.onscroll = () => {
+    if(window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+        }else {
+        document.querySelector('.header').classList.remove('active')
+    }
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active')
+}
+
+window.onload = () =>{
 
     if(window.scrollY > 0) {
         document.querySelector(".header").classList.add('active');
@@ -14,6 +32,4 @@ window.onscroll = () =>{
         document.querySelector(".header").classList.remove('active');
     }
 
-    menu.classList.remove('fa-times');
-    navbar.classList.add('active');
 }
